547f70e45d2da576424458f92b66cc89fc9fb9f2
OBigVee <adedejivictor98@gmail.com>
2022-05-03 00:08:58 +0100

    feat:
    -Add '1-simple_pagination.py' file
    script depcit a Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" """
+import csv
+import math
+from typing import List
+from unittest import result
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+    
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def index_range(self,page: int, page_size: int) -> tuple:
+        """returns a tuple of size two containing and an end index
+        corresponding to the range of indexes to return in a list for those particular
+        pagination parameters
+        """
+        startIndex = (page - 1) * page_size
+        endIndex = startIndex + page_size
+        return (startIndex, endIndex)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ get appropriate page of dataset"""
+        assert page > 0 or page_size > 0
+        # deconstructing
+        start, end = self.index_range(page, page_size)
+        
+        getData = self.dataset()
+
+        result = []
+        
+        if start >= len(getData):
+            return result
+        return getData[start:end]
+
