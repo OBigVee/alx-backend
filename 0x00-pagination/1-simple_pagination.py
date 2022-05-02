bc61c4a7b42c52233bcd8f094be35458508b4819
OBigVee <adedejivictor98@gmail.com>
2022-05-03 00:20:30 +0100

    feat:
    -Add '1-simple_pagination.py' file
    script depcit a Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-""" """
+"""Simple pagination"""
 import csv
 import math
 from typing import List
-from unittest import result
 
-def index_range(self,page: int, page_size: int) -> tuple:
-        """returns a tuple of size two containing and an end index
-        corresponding to the range of indexes to return in a list for those particular
-        pagination parameters
-        """
-        startIndex = (page - 1) * page_size
-        endIndex = startIndex + page_size
-        return (startIndex, endIndex)
+
+def index_range(self, page: int, page_size: int) -> tuple:
+    """returns a tuple of size two containing and an end index
+    corresponding to the range of indexes to return in a list for those particular
+    pagination parameters
+    """
+    startIndex = (page - 1) * page_size
+    endIndex = startIndex + page_size
+    return (startIndex, endIndex)
+
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
-    
+
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,14 +31,14 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ get appropriate page of dataset"""
+        """get appropriate page of dataset"""
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         # deconstructing
-        start, end = index_range(self, page , page_size)
-        
+        start, end = index_range(self, page, page_size)
+
         getData = self.dataset()
 
         result = []
@@ -48,4 +47,3 @@ class Server:
             return result
         else:
             return getData[start:end]
-
