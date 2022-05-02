4bd57c95852b5cdc131efecc3d7215758c250072
OBigVee <adedejivictor98@gmail.com>
2022-05-02 22:48:55 +0100

    feat:
    - Add '0-simple_helper_function.py' file
    script is a simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""simple helper function"""
+
+
+def index_range(page: int, page_size: int) -> None:
+    """returns a tuple of size two containing and an end index
+    corresponding to the range of indexes to return in a list for those particular
+    pagination parameters
+    """
+    startIndex = (page - 1) * page_size
+    endIndex = startIndex + page_size
+    return (startIndex, endIndex)
