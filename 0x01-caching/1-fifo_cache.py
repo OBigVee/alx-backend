559f99e65f18b57d5cdb4b2e89afa741be60050d
OBigVee <adedejivictor98@gmail.com>
2022-05-04 21:34:06 +0100

    - Add '1-fifo_cache.py' file file implements FIFO sequence caching algorithm

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""FIFO Caching"""
+
+from collections import OrderedDict
+
+baseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(baseCaching):
+    """inherits from BaseCaching"""
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+        if len(self.cache_data.values()) > baseCaching.MAX_ITEMS:
+            """discard the first item put in cache (FIFO)"""
+            first_key, first_item = self.cache_data.popitem(False)
+            print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """returns value in self.cache_Data linked to key"""
+        return self.cache_data.get(key, None)
