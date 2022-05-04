9a24dea15edb950c46763367140b37e5480e8111
OBigVee <adedejivictor98@gmail.com>
2022-05-04 11:52:18 +0100

    feat:
    -Add '0-basic_cache.py' file
    script inherits from BaseCaching in 'base_caching' file
    script cache data

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic Dict"""
+baseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(baseCaching):
+    """inherits from BaseCaching"""
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        try:
+            if key is not None or item is not None:
+                self.cache_data[key] = item
+        except Exception:
+            pass
+
+    def get(self, key):
+        """Get an item by key"""
+        return (
+            None
+            if key is None or self.cache_data.get(key) is None
+            else self.cache_data[key]
+        )
--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -10,7 +10,7 @@ class BaseCaching():
     MAX_ITEMS = 4
 
     def __init__(self):
-        """ Initiliaze
+        """ Initialize
         """
         self.cache_data = {}
 
