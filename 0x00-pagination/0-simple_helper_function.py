122008ea04ac34af57af7bf199f7cdc965f2eec3
OBigVee <adedejivictor98@gmail.com>
2022-05-06 11:42:11 +0100

    feat:
    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """simple helper function"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """returns a tuple of size two containing and an end index
     corresponding to the range of indexes to return in a list for those particular
     pagination parameters
     """
-
     startIndex = (page - 1) * page_size
     endIndex = startIndex + page_size
     return (startIndex, endIndex)
