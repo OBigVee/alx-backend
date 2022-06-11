3245554b0a574cf8f9d840c3088b0114e4b33e08
OBigVee <adedejivictor98@gmail.com>
2022-06-11 09:36:58 +0100

    :heavy_check_mark: Create the Job creator completed :heavey_check_mark:

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,7 +3,7 @@ import { createQueue } from 'kue'
 const queue = createQueue()
 const objJob = {
     phoneNumber: '07012345678',
-    message: 'Hello world, it\'s Redis creating a job for you ah ah!',
+    message: "Hello world, it's Redis creating a job for you ah ah!",
 }
 const job = queue.create('push_notification_code',
 objJob
