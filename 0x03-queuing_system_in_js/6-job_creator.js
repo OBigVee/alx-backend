e272b9b50b9009a84320f6b08f5930afb1b858d7
OBigVee <adedejivictor98@gmail.com>
2022-06-11 09:28:54 +0100

    :heavy_check_mark: Create the Job creator completed :heavey_check_mark:

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,18 @@
+import { createQueue } from 'kue'
+
+const queue = createQueue()
+const objJob = {
+    phoneNumber: '07012345678',
+    message: 'Hello world, it\'s Redis creating a job for you ah ah!',
+}
+const job = queue.create('push_notification_code',
+objJob
+).save((err)=>{
+    if(!err) console.log(`Notification job created: ${job.id}`)
+})
+
+job.on('compelte', ()=>{
+    console.log('Notification job completed')
+}).on('failed', (errorMessage)=>{
+    console.log('Notification job failed')
+})
