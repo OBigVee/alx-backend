7a91bf120f79427f0c45da59881a9afe47de3bb4
OBigVee <adedejivictor98@gmail.com>
2022-06-11 10:23:07 +0100

    :heavy_check_mark: task 8 completed - Task Track progress and errors with Kue : create the Job processor :heavy_check_mark:

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,39 @@
+/**
+ * Track progress and errors with Kue: Create the Job processor
+ */
+
+import { createQueue, Job } from 'kue'
+
+const queue = createQueue()
+
+const blackListedPhoneArray = [
+    4153518780,
+    4153518781
+]
+
+const sendNotification = (phoneNumber, message, job, done)=>{
+    let pending = 2
+    let total = 2
+    let sendInterval = setInterval(() =>{
+        if(total - pending <= total / 2){
+            job.progress(total - pending, total)
+        }
+
+        if(blackListedPhoneArray.includes(phoneNumber)){
+            done(new Error(`Phone number ${phoneNumber} is blacklisted`))
+            clearInterval(sendInterval);
+            return;
+        }
+
+        if(total === pending ){
+            console.log(`Sending notification to ${phoneNumber},
+            with message: ${message}`)
+        }
+        --pending || done()
+        pending || clearInterval(sendInterval)
+    }, 1000)
+}
+
+queue.process('push_notification_code_2', 2, (job, done)=>{
+    sendNotification(job.phoneNumber, job.data.message, job, done)
+})
