ce9902e27e7c55e7f40de92bcfe8eff7ad0ef3c8
obigvee <adedejivictor98@gmail.com>
2024-04-14 06:12:15 +0100

    :tada: :heavy_white_check: add (1-simple_helper_function.py and 1-main.py) files

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+    """function returns a tuple of size two containing a
+    start index and an end index corresponding to the
+    range of indexes to return in a list for those
+    particular pagination parameters"""
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self,
+                 page: int = 1,
+                 page_size: int = 10) -> List[List]:
+        """method takes two int args"""
+        
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+
+        # unpacking
+        start, end = index_range(self, page, page_size)
+        get_data = self.dataset()
+        result: List = []
+        # if the input args are out of range for the dataset,return an empty
+        # list
+        return get_data[start: end] if start < len(get_data) else result     
