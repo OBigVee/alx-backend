d4a5aa7d2b7d63d857b6ef19390d5b7dc9640e7a
obigvee <adedejivictor98@gmail.com>
2024-04-19 17:53:55 +0100

    :heavy_check_mark: Task 0 simple flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import render_template
+"""simple flask app"""
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
